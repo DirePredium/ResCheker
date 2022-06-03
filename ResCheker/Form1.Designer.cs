13d34c5bc0fd95d2771fee273a45258f7450f947
Mikhail_Izvarin <Maikllls43@gmail.com>
2022-06-03 12:38:09 +0300

    Добавил интерфейс

--- ResCheker/Form1.Designer.cs
+++ ResCheker/Form1.Designer.cs
@@ -29,13 +29,264 @@ namespace ResCheker
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader2 = new System.Windows.Forms.ColumnHeader();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.metroProgressBar1 = new MetroFramework.Controls.MetroProgressBar();
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel3 = new MetroFramework.Controls.MetroLabel();
+            this.metroProgressBar2 = new MetroFramework.Controls.MetroProgressBar();
+            this.metroLabel4 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel5 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel6 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel7 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel8 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel9 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel10 = new MetroFramework.Controls.MetroLabel();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 63);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(948, 559);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.listView1);
+            this.tabPage1.Controls.Add(this.comboBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 24);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(940, 487);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Характеристики";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(6, 35);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(928, 446);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Название";
+            this.columnHeader1.Width = 200;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Значение";
+            this.columnHeader2.Width = 200;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Процессор",
+            "Видеокарта",
+            "Чипсет",
+            "Батарея",
+            "Биос",
+            "Оперативная память",
+            "Кэш",
+            "USB",
+            "Диск",
+            "Логические диски",
+            "Клавиатура",
+            "Сеть",
+            "Пользователи"});
+            this.comboBox1.Location = new System.Drawing.Point(6, 6);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 23);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.metroLabel10);
+            this.tabPage2.Controls.Add(this.metroLabel9);
+            this.tabPage2.Controls.Add(this.metroLabel8);
+            this.tabPage2.Controls.Add(this.metroLabel7);
+            this.tabPage2.Controls.Add(this.metroLabel6);
+            this.tabPage2.Controls.Add(this.metroLabel5);
+            this.tabPage2.Controls.Add(this.metroLabel3);
+            this.tabPage2.Controls.Add(this.metroProgressBar2);
+            this.tabPage2.Controls.Add(this.metroLabel4);
+            this.tabPage2.Controls.Add(this.metroLabel2);
+            this.tabPage2.Controls.Add(this.metroProgressBar1);
+            this.tabPage2.Controls.Add(this.metroLabel1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 24);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(940, 531);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Загруженность";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.Location = new System.Drawing.Point(7, 21);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(77, 19);
+            this.metroLabel1.TabIndex = 0;
+            this.metroLabel1.Text = "Процессор";
+            // 
+            // metroProgressBar1
+            // 
+            this.metroProgressBar1.Location = new System.Drawing.Point(94, 9);
+            this.metroProgressBar1.Name = "metroProgressBar1";
+            this.metroProgressBar1.Size = new System.Drawing.Size(724, 45);
+            this.metroProgressBar1.TabIndex = 2;
+            // 
+            // metroLabel2
+            // 
+            this.metroLabel2.AutoSize = true;
+            this.metroLabel2.Location = new System.Drawing.Point(824, 21);
+            this.metroLabel2.Name = "metroLabel2";
+            this.metroLabel2.Size = new System.Drawing.Size(20, 19);
+            this.metroLabel2.TabIndex = 3;
+            this.metroLabel2.Text = "%";
+            // 
+            // metroLabel3
+            // 
+            this.metroLabel3.AutoSize = true;
+            this.metroLabel3.Location = new System.Drawing.Point(824, 87);
+            this.metroLabel3.Name = "metroLabel3";
+            this.metroLabel3.Size = new System.Drawing.Size(20, 19);
+            this.metroLabel3.TabIndex = 6;
+            this.metroLabel3.Text = "%";
+            // 
+            // metroProgressBar2
+            // 
+            this.metroProgressBar2.Location = new System.Drawing.Point(94, 75);
+            this.metroProgressBar2.Name = "metroProgressBar2";
+            this.metroProgressBar2.Size = new System.Drawing.Size(724, 45);
+            this.metroProgressBar2.TabIndex = 5;
+            // 
+            // metroLabel4
+            // 
+            this.metroLabel4.AutoSize = true;
+            this.metroLabel4.Location = new System.Drawing.Point(48, 87);
+            this.metroLabel4.Name = "metroLabel4";
+            this.metroLabel4.Size = new System.Drawing.Size(36, 19);
+            this.metroLabel4.TabIndex = 4;
+            this.metroLabel4.Text = "ОЗУ";
+            // 
+            // metroLabel5
+            // 
+            this.metroLabel5.AutoSize = true;
+            this.metroLabel5.Location = new System.Drawing.Point(7, 158);
+            this.metroLabel5.Name = "metroLabel5";
+            this.metroLabel5.Size = new System.Drawing.Size(94, 19);
+            this.metroLabel5.TabIndex = 7;
+            this.metroLabel5.Text = "Используется:";
+            // 
+            // metroLabel6
+            // 
+            this.metroLabel6.AutoSize = true;
+            this.metroLabel6.Location = new System.Drawing.Point(126, 158);
+            this.metroLabel6.Name = "metroLabel6";
+            this.metroLabel6.Size = new System.Drawing.Size(30, 19);
+            this.metroLabel6.TabIndex = 8;
+            this.metroLabel6.Text = "000";
+            // 
+            // metroLabel7
+            // 
+            this.metroLabel7.AutoSize = true;
+            this.metroLabel7.Location = new System.Drawing.Point(126, 188);
+            this.metroLabel7.Name = "metroLabel7";
+            this.metroLabel7.Size = new System.Drawing.Size(30, 19);
+            this.metroLabel7.TabIndex = 9;
+            this.metroLabel7.Text = "000";
+            // 
+            // metroLabel8
+            // 
+            this.metroLabel8.AutoSize = true;
+            this.metroLabel8.Location = new System.Drawing.Point(28, 188);
+            this.metroLabel8.Name = "metroLabel8";
+            this.metroLabel8.Size = new System.Drawing.Size(70, 19);
+            this.metroLabel8.TabIndex = 10;
+            this.metroLabel8.Text = "Доступно:";
+            // 
+            // metroLabel9
+            // 
+            this.metroLabel9.AutoSize = true;
+            this.metroLabel9.Location = new System.Drawing.Point(126, 219);
+            this.metroLabel9.Name = "metroLabel9";
+            this.metroLabel9.Size = new System.Drawing.Size(30, 19);
+            this.metroLabel9.TabIndex = 11;
+            this.metroLabel9.Text = "000";
+            // 
+            // metroLabel10
+            // 
+            this.metroLabel10.AutoSize = true;
+            this.metroLabel10.Location = new System.Drawing.Point(10, 219);
+            this.metroLabel10.Name = "metroLabel10";
+            this.metroLabel10.Size = new System.Drawing.Size(91, 19);
+            this.metroLabel10.TabIndex = 12;
+            this.metroLabel10.Text = "Установлено:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(966, 634);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "ResChecker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private MetroFramework.Controls.MetroLabel metroLabel10;
+        private MetroFramework.Controls.MetroLabel metroLabel9;
+        private MetroFramework.Controls.MetroLabel metroLabel8;
+        private MetroFramework.Controls.MetroLabel metroLabel7;
+        private MetroFramework.Controls.MetroLabel metroLabel6;
+        private MetroFramework.Controls.MetroLabel metroLabel5;
+        private MetroFramework.Controls.MetroLabel metroLabel3;
+        private MetroFramework.Controls.MetroProgressBar metroProgressBar2;
+        private MetroFramework.Controls.MetroLabel metroLabel4;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
+        private MetroFramework.Controls.MetroProgressBar metroProgressBar1;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
     }
 }
 
--- ResCheker/Form1.cs
+++ ResCheker/Form1.cs
@@ -7,15 +7,162 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Management;
+using MetroFramework.Forms;
 
 namespace ResCheker
 {
-    public partial class Form1 : Form
+    public partial class Form1 : MetroForm
     {
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void GetHardWareInfo(string key, ListView list)
+        {
+            list.Items.Clear();
+
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT * FROM " + key);
+
+            try
+            {
+                foreach (ManagementObject obj in searcher.Get())
+                {
+                    ListViewGroup listViewGroup;
+
+                    try
+                    {
+                        listViewGroup = list.Groups.Add(obj["Name"].ToString(), obj["Name"].ToString());
+                    }
+                    catch (Exception ex)
+                    {
+                        listViewGroup = list.Groups.Add(obj.ToString(), obj.ToString());
+                    }
+
+                    if (obj.Properties.Count == 0)
+                    {
+                        MessageBox.Show("Не удалось получить информацию", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                        return;
+                    }
+
+                    foreach (PropertyData data in obj.Properties)
+                    {
+                        ListViewItem item = new ListViewItem(listViewGroup);
+
+                        if (list.Items.Count % 2 != 0)
+                        {
+                            item.BackColor = Color.White;
+                        }
+                        else
+                        {
+                            item.BackColor = Color.WhiteSmoke;
+                        }
+
+                        item.Text = data.Name;
+
+                        if(data.Value != null && !string.IsNullOrEmpty(data.Value.ToString()))
+                        {
+                            switch (data.Value.GetType().ToString())
+                            {
+                                case "System.String[]":
+                                    string[] stringData = data.Value as string[];
+
+                                    string resStr1 = String.Empty;
+
+                                    foreach (string s in stringData)
+                                    {
+                                        resStr1 += $"{s} ";
+                                    }
+
+                                    item.SubItems.Add(resStr1);
+                                    break;
+                                case "System.UInt16[]":
+                                    ushort[] uShortData = data.Value as ushort[];
+
+                                    string resStr2 = String.Empty;
+
+                                    foreach (ushort u in uShortData)
+                                    {
+                                        resStr2 += $"{Convert.ToString(u)} ";
+                                    }
+
+                                    item.SubItems.Add(resStr2);
+                                    break;
+                                default:
+
+                                    item.SubItems.Add(data.Value.ToString());
+                                    break;
+                            }
+
+                            list.Items.Add(item);
+                        }
+                    }
+                }
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string key = string.Empty;
+
+            switch (comboBox1.SelectedItem.ToString())
+            {
+                case "Процессор":
+                    key = "Win32_Processor";
+                    break;
+                case "Видеокарта":
+                    key = "Win32_VideoController";
+                    break;
+                case "Чипсет":
+                    key = "Win32_IDEController";
+                    break;
+                case "Батарея":
+                    key = "Win32_Battery";
+                    break;
+                case "Биос":
+                    key = "Win32_BIOS";
+                    break;
+                case "Оперативная память":
+                    key = "Win32_PhysicalMemory";
+                    break;
+                case "Кэш":
+                    key = "Win32_CacheMemory";
+                    break;
+                case "USB":
+                    key = "Win32_USBController";
+                    break;
+                case "Диск":
+                    key = "Win32_DiskDrive";
+                    break;
+                case "Логические диски":
+                    key = "Win32_LogicalDisk";
+                    break;
+                case "Клавиатура":
+                    key = "Win32_Keyboard";
+                    break;
+                case "Сеть":
+                    key = "Win32_NetworkAdapter";
+                    break;
+                case "Пользователи":
+                    key = "Win32_Account";
+                    break;
+                default:
+                    key = "Win32_Processor";
+                    break;
+
+            }
+            GetHardWareInfo(key, listView1);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.SelectedIndex = 0;
+        }
     }
 }
